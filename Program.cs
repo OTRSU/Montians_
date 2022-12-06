b5d53d1be4ae807f222392955b7bedb2dac3386d
OTRSU <shennew123@outlook.com>
2022-12-06 19:12:36 +0800

    hhhh

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace HelloWorldApplication
+{
+    class Test{
+        double length;
+        double width;
+        public void Acceptdetails(){
+            length = 65;
+            width = 87;
+        }
+        public double GetArea(){
+            return length * width;
+        }public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class ExecuteTest
+    {
+        static void Main(string[] args)
+        {
+            Test r = new Test();
+            r.Acceptdetails();
+            r.Display();
+        }
+    }
+}
